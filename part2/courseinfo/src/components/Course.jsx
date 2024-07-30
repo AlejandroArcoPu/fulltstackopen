a9468cc8c9f6757c399b9c672206c50e8752e888
Alejandro Arco <alejandroarpu@gmail.com>
2024-07-30 22:29:37 +0200

    fix: only using course file

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,6 +1,22 @@
-import Header from './Header'
-import Part from './Part'
-import Total from './Total'
+const Total = ({parts}) => {
+    return(
+        <p><strong>total of {parts.reduce((partialSum, parts) => 
+            partialSum + parts.exercises, 0)} exercises</strong></p>
+    )
+}
+
+const Header = ({text}) => {
+    return(
+        <h1>{text}</h1>
+    )
+}
+
+const Part = ({name,exercises}) => {
+    return(
+        <p>{name} {exercises}</p>
+    )
+}
+
 
 const Course = ({course}) => {
     return (
