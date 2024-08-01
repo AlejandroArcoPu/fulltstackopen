1951ce0beac04c17106e61cec37bfb1de45c16d8
Alejandro Arco <alejandroarpu@gmail.com>
2024-08-01 21:05:29 +0200

    feat: add 2.10 exercise external components

--- part2/phoneagenda/src/App.jsx
+++ part2/phoneagenda/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -18,7 +21,7 @@ const App = () => {
       number: newNumber,
       id: persons.length + 1
     }
-    persons.some(person => person.name === personObject.name) 
+    persons.some(person => person.name === personObject.name.trim()) 
     ? alert(`${newName} is already added to phonebook`) 
     : setPersons(persons.concat(personObject))
     
@@ -26,34 +29,22 @@ const App = () => {
     setNewNumber('')
   }
 
+  const handleOnChangeName = (event) => setNewName(event.target.value)
+  
+  const handleOnChangeNumber = (event) => setNewNumber(event.target.value)
+
   const personsToShow = newFilter.length !== 0 
   ? persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
   : persons
-
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      filter shown with <input value={newFilter} onChange={(event) => setNewFilter(event.target.value)}/>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {
-          personsToShow.map(person => 
-            <li key={person.id}>{person.name} {person.number}</li>
-          )
-        }
-      </ul>
+      <Filter filter={newFilter} onChange={(event) => setNewFilter(event.target.value)}/>
+      <h3>add a new</h3>
+      <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} onChangeName={handleOnChangeName} onChangeNumber={handleOnChangeNumber}/>
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow}/>
     </div>
   )
 }
