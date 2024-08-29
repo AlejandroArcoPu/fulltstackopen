86cb0fc1b3f59709dc20662b4665ced3e7167b1b
Alejandro Arco <alejandroarpu@gmail.com>
2024-08-29 22:24:22 +0200

    feat: add initial commit blogs exercises 4.1, 4.2

---
+++ part4/blogs/app.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const cors = require('cors')
+const express = require('express')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blog') 
+const middleware = require('./utils/middleware')
+const morgan = require('morgan')
+const app = express()
+
+logger.info('connecting to',config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+.then(() => {
+    logger.info('connected to MongoDB')
+})
+.catch((error) => {
+    logger.error('error connecting to MongoDB', error)
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.use(morgan('tiny'))
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
