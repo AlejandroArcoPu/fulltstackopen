7eda433efdecf608531c02488f52d22b012b8275
Alejandro Arco <alejandroarpu@gmail.com>
2024-09-03 12:15:57 +0200

    feat: add delete id exercise 4.13

--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -7,7 +7,7 @@ const api = supertest(app)
 const Blog = require('../models/blog')
 const helper = require('./test_helpers')
 
-describe('api blogs', () => {
+describe('when some blogs are saved', () => {
 
   beforeEach(async () => {
     await Blog.deleteMany({})
@@ -33,84 +33,86 @@ describe('api blogs', () => {
     const response = await api.get('/api/blogs')
     assert('id' in response.body[0])
   })
+  describe('addition of a new blog', () => {
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        id: '1a234b567b89a676234d17fa',
+        title: 'Alejandros book',
+        author: 'Alejandro Arco',
+        url: 'https://alejandrosbook.com',
+        likes: 10,
+      }
 
-  test('a valid blog can be added', async () => {
-    const newBlog = {
-      id: '1a234b567b89a676234d17fa',
-      title: 'Alejandros book',
-      author: 'Alejandro Arco',
-      url: 'https://alejandrosbook.com',
-      likes: 10,
-    }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type',/application\/json/)
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type',/application\/json/)
+      const blogsEnd = await helper.blogsInBd()
 
-    const blogsEnd = await helper.blogsInBd()
+      assert.strictEqual(blogsEnd.length,helper.initialBlogs.length + 1)
+      const titleList = blogsEnd.map(b => b.title)
+      assert(titleList.includes('Alejandros book'))
+    })
 
-    assert.strictEqual(blogsEnd.length,helper.initialBlogs.length + 1)
-    const titleList = blogsEnd.map(b => b.title)
-    assert(titleList.includes('Alejandros book'))
-  })
+    test('a blog without likes get 0 as default', async () => {
+      const noLikesBlog = {
+        id: '1a234b567b89a676234d17fa',
+        title: 'Alejandros book',
+        author: 'Alejandro Arco',
+        url: 'https://alejandrosbook.com'
+      }
 
-  test('a blog without likes get 0 as default', async () => {
-    const noLikesBlog = {
-      id: '1a234b567b89a676234d17fa',
-      title: 'Alejandros book',
-      author: 'Alejandro Arco',
-      url: 'https://alejandrosbook.com'
-    }
+      await api
+        .post('/api/blogs')
+        .send(noLikesBlog)
+        .expect(201)
+        .expect('Content-Type',/application\/json/)
 
-    await api
-      .post('/api/blogs')
-      .send(noLikesBlog)
-      .expect(201)
-      .expect('Content-Type',/application\/json/)
+      const blogsEnd = await helper.blogsInBd()
+      assert.strictEqual(blogsEnd.length,helper.initialBlogs.length + 1)
+      const newBlogLikes = blogsEnd.find(b =>
+        b.title === 'Alejandros book').likes
+      assert.strictEqual(newBlogLikes, 0)
+    })
 
-    const blogsEnd = await helper.blogsInBd()
-    assert.strictEqual(blogsEnd.length,helper.initialBlogs.length + 1)
-    const newBlogLikes = blogsEnd.find(b =>
-      b.title === 'Alejandros book').likes
-    assert.strictEqual(newBlogLikes, 0)
-  })
+    test('a blog without title can not be added', async () => {
+      const invalidTitleBlog = {
+        id: '1a234b567b89a676234d17fa',
+        author: 'Alejandro Arco',
+        url: 'https://alejandrosbook.com',
+        likes: 10,
+      }
 
-  test('a blog without title can not be added', async () => {
-    const invalidTitleBlog = {
-      id: '1a234b567b89a676234d17fa',
-      author: 'Alejandro Arco',
-      url: 'https://alejandrosbook.com',
-      likes: 10,
-    }
+      await api
+        .post('/api/blogs')
+        .send(invalidTitleBlog)
+        .expect(400)
 
-    await api
-      .post('/api/blogs')
-      .send(invalidTitleBlog)
-      .expect(400)
+      const blogsEnd = await helper.blogsInBd()
 
-    const blogsEnd = await helper.blogsInBd()
+      assert.strictEqual(blogsEnd.length,helper.initialBlogs.length)
+    })
 
-    assert.strictEqual(blogsEnd.length,helper.initialBlogs.length)
-  })
+    test('a blog without url can not be added', async () => {
+      const invalidUrlBlog = {
+        title: 'Alejandros book',
+        id: '1a234b567b89a676234d17fa',
+        author: 'Alejandro Arco',
+        likes: 10,
+      }
 
-  test('a blog without url can not be added', async () => {
-    const invalidUrlBlog = {
-      title: 'Alejandros book',
-      id: '1a234b567b89a676234d17fa',
-      author: 'Alejandro Arco',
-      likes: 10,
-    }
+      await api
+        .post('/api/blogs')
+        .send(invalidUrlBlog)
+        .expect(400)
 
-    await api
-      .post('/api/blogs')
-      .send(invalidUrlBlog)
-      .expect(400)
+      const blogsEnd = await helper.blogsInBd()
 
-    const blogsEnd = await helper.blogsInBd()
+      assert.strictEqual(blogsEnd.length,helper.initialBlogs.length)
+    })
 
-    assert.strictEqual(blogsEnd.length,helper.initialBlogs.length)
   })
 
   after(async () => {
--- part4/blogs/utils/middleware.js
+++ part4/blogs/utils/middleware.js
@@ -6,9 +6,8 @@ const unknownEndpoint = (request,response) => {
 
 const errorHandler = (error,request,response,next) => {
   logger.error(error)
-  if(error.name === 'ValidationError'){
-    return response.status(400).json({ error: error.message })
-  }
+  if(error.name === 'ValidationError') return response.status(400).json({ error: error.message })
+  if(error.name === 'CastError') return response.status(400).json({ error: 'ObjectId format is wrong' })
   next(error)
 }
 
