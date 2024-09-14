3886d24d823ddf4631ea1ba8a2cb80f13ca636d9
Alejandro Arco <alejandroarpu@gmail.com>
2024-09-14 17:09:23 +0200

    feat: add exercise 5.1 with blog and login form

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import LoginForm from './components/LoginForm'
+import Blog from './components/Blog'
 import loginService from './services/login'
+import blogsService from './services/blogs'
 
 function App() {
 
   const [username,setUsername] = useState('')
   const [password,setPassword] = useState('')
   const [user,setUser] = useState(null)
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const data = await blogsService.getAll()
+      setBlogs(data)
+    }
+    fetchBlogs()
+  }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -23,11 +34,20 @@ function App() {
     }
   }
   
+  if(user === null) {
+    return (
+      <div>
+        <LoginForm username={username} password={password} handleLogin={handleLogin} setUsername={setUsername} setPassword={setPassword}/>
+      </div>
+    )
+  }
   return (
-    <>
-      <LoginForm username={username} password={password} handleLogin={handleLogin} setUsername={setUsername} setPassword={setPassword}/>
-      <Blog></Blog>
-    </>
+    <div>
+      <h1>blogs</h1>
+      {blogs.map(blog => 
+        <Blog key={blog.id} blog={blog}/>
+      )}
+    </div>
   )
 }
 
--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,3 +1,7 @@
-const Blog = () => {
-    
-}
+const Blog = ({blog}) => (
+    <div>
+      {blog.title} {blog.author}
+    </div>  
+)
+
+export default Blog
