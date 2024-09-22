2afbada37fae6c35171ce09043e098d5dc789ca9
Alejandro Arco <alejandroarpu@gmail.com>
2024-09-22 18:44:53 +0200

    feat: add login e2e tests, beforeEach that clean user and blogs exercise 5.18

--- part5/blog-e2e/tests/blog_app.spec.js
+++ part5/blog-e2e/tests/blog_app.spec.js
@@ -1,7 +1,15 @@
 const { test, expect,beforeEach, describe } = require('@playwright/test');
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
+    beforeEach(async ({ page, request }) => {
+      await request.post('/api/testing/reset')
+      await request.post('/api/users', {
+        data: {
+            username: 'alejandroarpu',
+            name: 'Alejandro',
+            password: 'mysecret'
+        }
+      })
       await page.goto('/')
     })
   
@@ -12,7 +20,24 @@ describe('Blog app', () => {
     })
 
     describe('Login', () => {
-        test()
+        test('succeeds with correct credentials', async ({page}) => {
+            await page.getByTestId('username').fill('alejandroarpu')
+            await page.getByTestId('password').fill('mysecret')
+            await page.getByRole('button', {name: 'login'}).click()
+            await expect(page.getByText('Alejandro logged in')).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({page}) => {
+            await page.getByTestId('username').fill('alejandroarpu')
+            await page.getByTestId('password').fill('wrong')
+            await page.getByRole('button', {name: 'login'}).click()
+            const errorDiv = page.locator('div').filter({ hasText: /^wrong username or password$/})
+
+            await expect(errorDiv).toHaveCSS('border-style','solid')
+            await expect(errorDiv).toHaveCSS('color','rgb(255, 0, 0)')
+            await expect(page.getByText('wrong username or password')).toBeVisible()
+            await expect(page.getByText('Alejandro logged in')).not.toBeVisible()
+        })
     })
 
 })
